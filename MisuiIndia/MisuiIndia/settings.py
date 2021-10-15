465db3eb6368d17a5a882f16fec29537e03ecf7e
souravjha1111 <souravjha9599@gmail.com>
2021-10-16 00:27:19 +0530

    run collect static

--- MisuiIndia/MisuiIndia/settings.py
+++ MisuiIndia/MisuiIndia/settings.py
@@ -225,12 +225,9 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT  = [
-    os.path.join(BASE_DIR, 'static')
-]
+
+STATIC_ROOT  =    os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
